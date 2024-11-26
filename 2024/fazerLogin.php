759c4ef2d0fb18d18dcf104e47d672ceedaaed52
teodwro <teogreselle@gmail.com>
2024-11-26 09:01:47 -0300

    .

--- 2024/fazerLogin.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once("config.php");
-require_once("vendor/autoload.php");
-
-$controller = new Controller\LoginController();
-$controller->fazerLogin();
--- 2024/salvarRepresentante.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once("config.php");
-require_once("vendor/autoload.php");
-
-$controller = new Controller\RepresentanteController();
-$controller->save();

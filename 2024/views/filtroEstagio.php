d041537741a82b5ec6124ce8534412ae3d42e8f4
otaviw <vivicocarbone@gmail.com>
2024-12-12 04:32:45 -0300

    Update filtroEstagio.php

--- 2024/views/filtroEstagio.php
+++ 2024/views/filtroEstagio.php
@@ -1,25 +1,30 @@
+<?php include("includes/menu.php"); ?>
+
 <form method="POST" action="filtrarEstagio.php">
-    <h2>Filtrar Estágios</h2>
+    <h1>Filtrar Estágios</h1>
     
+
+    <a href="listaEstagios.php">Cancelar filtragem</a>
+
     <label for="curso_estagio">Curso:</label>
         <select name="filtro_curso" id="filtro_curso" required>
+            <option value="0" disabled selected>Selecione uma opção</option>
             <?php foreach ($cursos as $curso) { ?>
                 <option value="<?php echo $curso->getId(); ?>"><?php echo $curso->getNomeCurso(); ?></option>    
             <?php } ?>
         </select>
+    <label for="filtro_estudante">Nome Estudante:</label>
+    <input type="text" name="filtro_estudante" id="filtro_estudante" placeholder="Digite o nome do estudante">
 
-    
     <label for="filtro_data_inicio">Data Início:</label>
     <input type="date" name="filtro_data_inicio" id="filtro_data_inicio">
 
     <label for="filtro_data_fim">Data Fim:</label>
     <input type="date" name="filtro_data_fim" id="filtro_data_fim">
     
-    <label for="filtro_estudante">Nome Estudante:</label>
-    <input type="text" name="filtro_estudante" id="filtro_estudante" placeholder="Digite o nome do estudante">
-    
     <label for="filtro_empresa">Nome Empresa:</label>
     <select name="filtro_empresa" id="filtro_empresa" required>
+        <option value="0" disabled selected>Selecione uma opção</option>
             <?php foreach ($empresas as $empresa) { ?>
                 <option value="<?php echo $empresa->getId(); ?>"><?php echo $empresa->getNomeEmpresa(); ?></option>    
             <?php } ?>
@@ -27,16 +32,18 @@
     
     <label for="filtro_professor">Nome Professor Orientador:</label>
     <select name="filtro_professor" id="filtro_professor" required>
-            <?php foreach ($professores as $professor) { ?>
-                <option value="<?php echo $professor->getId(); ?>"><?php echo $professor->getNomeProfessor(); ?></option>    
-            <?php } ?>
+        <option value="0" disabled selected>Selecione uma opção</option>
+        <?php foreach ($professores as $professor) { ?>
+            <option value="<?php echo $professor->getId(); ?>"><?php echo $professor->getNomeProfessor(); ?></option>    
+        <?php } ?>
     </select>
     
     <label for="filtro_cidade">Cidade:</label>
     <select name="filtro_cidade" id="filtro_cidade" required>
-            <?php foreach ($cidades as $cidades) { ?>
-                <option value="<?php echo $cidades->getId(); ?>"><?php echo $cidades->getNome(); ?></option>    
-            <?php } ?>
+        <option value="0" disabled selected>Selecione uma opção</option>
+        <?php foreach ($cidades as $cidades) { ?>
+            <option value="<?php echo $cidades->getId(); ?>"><?php echo $cidades->getNome(); ?></option>    
+        <?php } ?>
     </select>
 
     
